5b9c2d4755c7e538d4f69e9994402a5d78078123
Brittany Walker <breneewalker572@gmail.com>
2021-09-08 08:49:53 -0400

    Initial commit

--- src/components/ConfigScreen.tsx
+++ src/components/ConfigScreen.tsx
@@ -1,25 +1,36 @@
-import React, { useCallback, useState, useEffect } from 'react';
-import { AppExtensionSDK } from '@contentful/app-sdk';
-import { Heading, Form, Workbench, Paragraph } from '@contentful/forma-36-react-components';
+import React from 'react'
+import { AppExtensionSDK } from '@contentful/app-sdk'
+import {
+  Heading,
+  Form,
+  Workbench,
+  Paragraph,
+  TextField,
+} from '@contentful/forma-36-react-components'
 import { css } from 'emotion';
+import logo from '../assets/logo.png'
 
-export interface AppInstallationParameters {}
-
-interface ConfigProps {
-  sdk: AppExtensionSDK;
+export interface AppInstallationParameters {
+  apiKey: string
 }
 
-const Config = (props: ConfigProps) => {
-  const [parameters, setParameters] = useState<AppInstallationParameters>({});
+interface ConfigProps {
+  sdk?: AppExtensionSDK
+}
 
-  const onConfigure = useCallback(async () => {
+const Config = ({ sdk }: ConfigProps) => {
+  const [parameters, setParameters] = React.useState<AppInstallationParameters>(
+    { apiKey: '' }
+  )
+
+  const onConfigure = React.useCallback(async () => {
     // This method will be called when a user clicks on "Install"
     // or "Save" in the configuration screen.
     // for more details see https://www.contentful.com/developers/docs/extensibility/ui-extensions/sdk-reference/#register-an-app-configuration-hook
 
     // Get current the state of EditorInterface and other entities
     // related to this app installation
-    const currentState = await props.sdk.app.getCurrentState();
+    const currentState = await sdk?.app.getCurrentState()
 
     return {
       // Parameters to be persisted as the app configuration.
@@ -27,40 +38,63 @@ const Config = (props: ConfigProps) => {
       // In case you don't want to submit any update to app
       // locations, you can just pass the currentState as is
       targetState: currentState,
-    };
-  }, [parameters, props.sdk]);
+    }
+  }, [parameters, sdk])
 
-  useEffect(() => {
+  React.useEffect(() => {
     // `onConfigure` allows to configure a callback to be
     // invoked when a user attempts to install the app or update
     // its configuration.
-    props.sdk.app.onConfigure(() => onConfigure());
-  }, [props.sdk, onConfigure]);
+    sdk?.app.onConfigure(() => onConfigure())
+  }, [sdk, onConfigure])
 
-  useEffect(() => {
-    (async () => {
+  React.useEffect(() => {
+    ;(async () => {
       // Get current parameters of the app.
       // If the app is not installed yet, `parameters` will be `null`.
-      const currentParameters: AppInstallationParameters | null = await props.sdk.app.getParameters();
+      const currentParameters: AppInstallationParameters | null =
+        (await sdk?.app.getParameters()) || null
 
       if (currentParameters) {
-        setParameters(currentParameters);
+        setParameters(currentParameters)
       }
 
       // Once preparation has finished, call `setReady` to hide
       // the loading screen and present the app to a user.
-      props.sdk.app.setReady();
+      sdk?.app.setReady()
     })()
-  }, [props.sdk])
+  }, [sdk])
+
+  function updateApiKey(event: React.ChangeEvent<HTMLInputElement>) {
+    setParameters((prevParams) => ({
+      ...prevParams,
+      apiKey: event.target.value.trim(),
+    }))
+  }
 
   return (
-    <Workbench className={css({ margin: '80px' })}>
-      <Form>
-        <Heading>App Config</Heading>
-        <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
+    <Workbench className='giphyConfig'>
+      <Form className='giphyForm'>
+        <div>
+          <Heading>Connect GIPHY</Heading>
+          <Paragraph className={css({ marginBottom: '20px', marginTop: '20px' })}>Enter your API Key for the GIPHY API.</Paragraph>
+          <TextField
+            id="apiKey"
+            name="apiKey"
+            labelText="GIPHY API Key:"
+            textInputProps={{ width: 'large' }}
+            value={parameters.apiKey}
+            onChange={updateApiKey}
+          />
+          </div>
+          <img className='configGif' src="https://media.giphy.com/media/hmVVRM1uV7vYA/giphy.gif?cid=ecf05e47rxig1yga9p64g7zkey7so6ecwi9sb72skv8ajtwn&rid=giphy.gif&ct=g" alt="Tina Fey giving herself a high five" />
+          <div className='giphyCredit'>
+            <Paragraph className='giphyAttribution'>Powered by</Paragraph>
+            <img className='giphyLogo' src={logo} alt="GIPHY logo" />
+          </div>
       </Form>
     </Workbench>
-  );
+  )
 }
 
-export default Config;
+export default Config 
--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,87 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { useEffect, useState } from 'react';
+import { FormLabel, TextInput, Button, IconButton } from '@contentful/forma-36-react-components';
 import { FieldExtensionSDK } from '@contentful/app-sdk';
-
+import { AppInstallationParameters } from './ConfigScreen'
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
 const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+
+  const params = props.sdk.parameters.installation as AppInstallationParameters
+  const [searchTerm, setSearchTerm] = useState("");
+  const [gif, setGif] = useState("");
+
+  const searchGif = () => {
+    if (searchTerm.length > 0) {
+      fetch(`https://api.giphy.com/v1/gifs/search?api_key=${params.apiKey}&limit=1&offset=0&q=${searchTerm}`)
+        .then((result) => {
+          return result.json();
+        })
+        .then((result) => {
+          setGif(result.data.map((gif: { images: { fixed_height: { url: any; }; }; }) => {
+            const gifURL = gif.images.fixed_height.url;
+            props.sdk.field.setValue(gifURL)
+            return gifURL;
+          }))
+        })
+        .catch(() => {
+          console.log("Oops, we could not find any results.");
+        })
+    }
+  }
+
+  const deleteGif = () => {
+    props.sdk.field.setValue("")
+    setGif("");
+  }
+
+  useEffect(() => {
+    props.sdk.window.startAutoResizer();
+    setGif(props.sdk.field.getValue());
+  }, [props.sdk.window, props.sdk.field]);
+  return (
+    <>
+      <div className="gifSearch">
+        <FormLabel htmlFor="Giphy Search">Search for a Gif</FormLabel>
+        <div className="gifSearchInput">
+          <TextInput
+            className="gifSearchBox"
+            labelText="Search for a Gif"
+            name="Giphy Search"
+            width="medium"
+            id="giphySearch"
+            value={searchTerm}
+            onChange={e => { setSearchTerm(e.target.value) }}
+          />
+          <Button buttonType="primary" onClick={searchGif}>Search</Button>
+        </div>
+      </div>
+      {gif &&
+      <div className="gifResult">
+        <div className="gifImage">
+          <img src={gif} alt="Gif"/>
+          <IconButton
+            className="gifDeleteButton"
+            testId="cf-ui-button-actions"
+            buttonType="negative"
+            label="Entry actions"
+            iconProps={{ icon: 'Close', size: 'large' }}
+            onClick={deleteGif}
+          />
+        </div>
+      </div>
+     }
+      <FormLabel htmlFor="Giphy URL">Gif URL (read only)</FormLabel>
+      <TextInput
+        labelText="GiphyURL"
+        name="Giphy URL"
+        id="giphyURL"
+        disabled
+        value={gif}
+      />
+    </>
+  );
 };
 
 export default Field;
